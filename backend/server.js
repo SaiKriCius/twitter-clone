abb8954320ef4cb3630abe84f6c84654a397ad44
SaiKriCius <jatin6410kumar@gmail.com>
2025-11-26 14:26:27 +0530

    1-post routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import {v2 as cloudinary} from "cloudinary";
 
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 
 import connectMongoDB from "./db/connentMongoDB.js";
 
@@ -26,7 +27,8 @@ app.use(express.urlencoded({ extended: true })); //to parse form data
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
+app.use("/api/users", userRoutes); 
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is up and rrunning on port ${PORT}`);
