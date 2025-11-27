944bedd3f83c42e67c22287a15822e1b229fbd91
SaiKriCius <jatin6410kumar@gmail.com>
2025-11-27 13:27:13 +0530

    3-post controller changes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -40,6 +40,7 @@ export const followUnfollowUser = async (req, res) => {
             //unfollow the user
             await User.findByIdAndUpdate(id,{$pull:{followers:req.user._id}}); 
             await User.findByIdAndUpdate(req.user._id,{$pull:{following:id}});
+            
             res.status(200).json({message:"User unfollowed successfully"});
         }
         else{
