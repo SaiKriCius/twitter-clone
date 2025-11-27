17c3245f78dd21a803faf8d2c99f72ae67b5e3ab
SaiKriCius <jatin6410kumar@gmail.com>
2025-11-27 14:19:08 +0530

    3-last changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -51,11 +51,11 @@ function App() {
         {/* common component, bc it's not wrapped with routes */}
         {authUser && <Sidebar/>}
 			<Routes>
-				<Route path='/' element={authUser ? <HomePage /> : <Navigate to="/LoginPage"  />} />
+				<Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login"  />} />
 				<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
 				<Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
-				<Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to="/LoginPage" />} />
-				<Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to="/LoginPage" />} />
+				<Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to="/login" />} />
+				<Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
 			</Routes>
         {authUser && <RightPanel/>}
         <Toaster/>
